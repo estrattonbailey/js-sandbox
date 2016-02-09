510348c291bf3cba7e44e5ebbd224b02be6b8bcc
estrattonbailey <ericstrattonbailey@gmail.com>
2016-02-09 14:28:24 -0500

    Getter/setter observable working, one level deep.

--- app.js
+++ app.js
@@ -44,64 +44,88 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	window.proto = {
-	  data: [],
-	  listeners: [],
-	  set(obj){
-	    var exists = this._find(this);
-
-	    // console.log(this)
-	    // console.log(exists)
-
-	    if (!!exists && exists.length > 0 ){
-	      Object.keys(obj).forEach(function(k){
-	        exists[0][k] = obj[k] 
-	      });
-	    } else {
-	      this.data.push(obj);
+	var proto = {
+	  // cache of callback functions
+		listeners: {},
+	  // assigns callbacks to keys on object
+		watch: function(key, cb){
+			var key = key || 'all';
+			
+	    if (!this.listeners[key]){
+	      this.listeners[key] = {
+	        queue: []
+	      };
 	    }
-	  },
-	  watch: function(key, cb){},
-	  _find: function(obj){
-	    console.log(obj)
-	    this.data.filter(function(o){
-	      console.log(o)
-	      return o.id === obj.id;
-	    })
-	  }
+			
+			this.listeners[key].queue.push(cb);
+		},
+	  // run all callbacks in specific queue
+		publish: function(key, val){
+			this.listeners[key].queue.forEach(function(fn, i){
+				fn(val);
+			});
+		}
 	}
 
+	/**
+	 * Create blank object with proto methods.
+	 * Create getters and setters for each property.
+	 * Setter fires this.publish() callback function.
+	 * @param {object} obj Any object the user wants to create
+	 */
 	function observable(obj){
-	  var _o = Object.create(proto, {
-	    id: {
-	      writable: false,
-	      enumerable: false,
-	      value: new Date().getTime()
-	    }
-	  });
-
-	  Object.keys(obj).forEach(function(k){
-	    _o[k] = obj[k]
-	  });
-
-	  _o.set()
-
-	  return _o;
+		var _o = Object.create(proto);
+		
+		Object.keys(obj).forEach(function(key){
+			_o[key] = obj[key];
+			
+			Object.defineProperty(_o, key, { 
+				set: function(val){ 
+					Object.defineProperty(this, key, {
+						value: val
+					});
+					
+					this.publish(key, val);
+				},
+				get: function(){
+	        return obj[key]
+				}
+			});
+		});
+		
+		return _o;
 	}
 
-	const user = observable({
-	  content: 'Hello world!',
-	  modified: 'Tuesday'
+	// Create new observable object
+	var user = observable({
+		firstName: 'Eric',
+		lastName: 'Bailey',
+		meta: {
+			birthday: '1992-02-14'
+		}
 	});
 
+	// Add callbacks for a few properties
+	user.watch('firstName', function(val){
+		console.log(val)
+	});
+	user.watch('firstName', function(val){
+		console.log(val+' '+user.lastName)
+	});
+	user.watch('meta', function(val){
+		console.log(val)
+	});
 
+	// Set new property values
 	setTimeout(function(){
-	  user.set({
-	    date: 'Date!'
-	  });
+	  user.firstName = 'Ryan';
+	  user.meta = {
+	    birthday: '1990-05-08'
+	  };
 	}, 1000);
 
-	// console.dir(user)
+	// Result
+	console.dir(user);
 
 
 /***/ }
