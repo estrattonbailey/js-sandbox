b6cb49e0b011d624f95a7eb8f9a616e852f6bae4
estrattonbailey <ericstrattonbailey@gmail.com>
2016-02-18 09:59:19 -0500

    Form testing

--- app.js
+++ app.js
@@ -42,90 +42,170 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
+	var lookout = __webpack_require__(1);
+
+	function form(args){
+	  var outer, inputs, 
+	      target = {};
+
+	  outer = document.querySelector(args.outer);
+	  inputs = Array.prototype.slice.call(outer.querySelectorAll(args.inputs));
+
+	  inputs.forEach(function(el, i){
+	    target[el.getAttribute('name')] = el.value;
+	  });
+
+	  target = lookout(target);
+
+	  return target;
+	}
+
+	var test = form({
+	  outer: '.js-form',
+	  inputs: '.js-input'
+	});
+
+	console.dir(test);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function(f){if(true){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}(g.index || (g.index = {})).js = f()}})(function(){var define,module,exports;
+	var isObj = __webpack_require__(2);
+
+	/**
+	 * Added as a prototype to each 
+	 * object created with Lookout()
+	 */
 	var proto = {
 	  // cache of callback functions
-		listeners: {},
+	  listeners: {},
 	  // assigns callbacks to keys on object
-		watch: function(key, cb){
-			var key = key || 'all';
-			
+	  watch: function(key, cb){
+	    var key = key || 'all'; // TODO: write 'all' functionality 
+
 	    if (!this.listeners[key]){
 	      this.listeners[key] = {
 	        queue: []
 	      };
 	    }
-			
-			this.listeners[key].queue.push(cb);
-		},
+
+	    this.listeners[key].queue.push(cb);
+	  },
 	  // run all callbacks in specific queue
-		publish: function(key, val){
-			this.listeners[key].queue.forEach(function(fn, i){
-				fn(val);
-			});
-		}
+	  publish: function(key, val){
+	    // if a callback hasn't been specified yet, return
+	    if (!this.listeners[key]) return;
+
+	    // run callback with changed value as param
+	    this.listeners[key].queue.forEach(function(fn, i){
+	      fn(val);
+	    });
+	  }
 	}
 
 	/**
 	 * Create blank object with proto methods.
+	 * Set props data bucket equal to passed source object.
 	 * Create getters and setters for each property.
 	 * Setter fires this.publish() callback function.
-	 * @param {object} obj Any object the user wants to create
+	 * @param {object} source Any object the user wants to create
 	 */
-	function observable(obj){
-		var _o = Object.create(proto);
-		
-		Object.keys(obj).forEach(function(key){
-			_o[key] = obj[key];
-			
-			Object.defineProperty(_o, key, { 
-				set: function(val){ 
-					Object.defineProperty(this, key, {
-						value: val
-					});
-					
-					this.publish(key, val);
-				},
-				get: function(){
-	        return obj[key]
-				}
-			});
-		});
-		
-		return _o;
+	function Lookout(source){
+	  var target;
+
+	  if (!isObj(source)) return console.log('%cPassed parameter ('+source+') is not an object.', 'background-color:#ff4567;color:#333333');
+
+	  target = Object.create(proto, {
+	    props: {
+	      value: source 
+	    }
+	  });
+
+	  Object.keys(source).forEach(function(key){
+	    Object.defineProperty(target, key, { 
+	      set: function(val){ 
+	        this.props[key] = val;
+	        this.publish(key, val);
+	      },
+	      get: function(){
+	        return this.props[key]
+	      }
+	    });
+	  });
+
+	  return target;
 	}
 
-	// Create new observable object
-	var user = observable({
-		firstName: 'Eric',
-		lastName: 'Bailey',
-		meta: {
-			birthday: '1992-02-14'
-		}
+	return Lookout;
+
 	});
 
-	// Add callbacks for a few properties
-	user.watch('firstName', function(val){
-		console.log(val)
-	});
-	user.watch('firstName', function(val){
-		console.log(val+' '+user.lastName)
-	});
-	user.watch('meta', function(val){
-		console.log(val)
-	});
 
-	// Set new property values
-	setTimeout(function(){
-	  user.firstName = 'Ryan';
-	  user.meta = {
-	    birthday: '1990-05-08'
-	  };
-	}, 1000);
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
 
-	// Result
-	console.dir(user);
+	/*!
+	 * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
+	 *
+	 * Copyright (c) 2014-2015, Jon Schlinkert.
+	 * Licensed under the MIT License.
+	 */
+
+	'use strict';
+
+	var isObject = __webpack_require__(3);
+
+	function isObjectObject(o) {
+	  return isObject(o) === true
+	    && Object.prototype.toString.call(o) === '[object Object]';
+	}
+
+	module.exports = function isPlainObject(o) {
+	  var ctor,prot;
+	  
+	  if (isObjectObject(o) === false) return false;
+	  
+	  // If has modified constructor
+	  ctor = o.constructor;
+	  if (typeof ctor !== 'function') return false;
+	  
+	  // If has modified prototype
+	  prot = ctor.prototype;
+	  if (isObjectObject(prot) === false) return false;
+	  
+	  // If constructor does not have an Object-specific method
+	  if (prot.hasOwnProperty('isPrototypeOf') === false) {
+	    return false;
+	  }
+	  
+	  // Most likely a plain Object
+	  return true;
+	};
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	/*!
+	 * isobject <https://github.com/jonschlinkert/isobject>
+	 *
+	 * Copyright (c) 2014-2015, Jon Schlinkert.
+	 * Licensed under the MIT License.
+	 */
+
+	'use strict';
+
+	module.exports = function isObject(val) {
+	  return val != null && typeof val === 'object'
+	    && !Array.isArray(val);
+	};
 
 
 /***/ }
--- src/index.js
+++ src/index.js
@@ -1,82 +1,24 @@
-var proto = {
-  // cache of callback functions
-	listeners: {},
-  // assigns callbacks to keys on object
-	watch: function(key, cb){
-		var key = key || 'all';
-		
-    if (!this.listeners[key]){
-      this.listeners[key] = {
-        queue: []
-      };
-    }
-		
-		this.listeners[key].queue.push(cb);
-	},
-  // run all callbacks in specific queue
-	publish: function(key, val){
-		this.listeners[key].queue.forEach(function(fn, i){
-			fn(val);
-		});
-	}
+var lookout = require('../../lookoutjs/dist/index.js');
+
+function form(args){
+  var outer, inputs, 
+      target = {};
+
+  outer = document.querySelector(args.outer);
+  inputs = Array.prototype.slice.call(outer.querySelectorAll(args.inputs));
+
+  inputs.forEach(function(el, i){
+    target[el.getAttribute('name')] = el.value;
+  });
+
+  target = lookout(target);
+
+  return target;
 }
 
-/**
- * Create blank object with proto methods.
- * Create getters and setters for each property.
- * Setter fires this.publish() callback function.
- * @param {object} obj Any object the user wants to create
- */
-function observable(obj){
-	var _o = Object.create(proto);
-	
-	Object.keys(obj).forEach(function(key){
-		_o[key] = obj[key];
-		
-		Object.defineProperty(_o, key, { 
-			set: function(val){ 
-				Object.defineProperty(this, key, {
-					value: val
-				});
-				
-				this.publish(key, val);
-			},
-			get: function(){
-        return obj[key]
-			}
-		});
-	});
-	
-	return _o;
-}
-
-// Create new observable object
-var user = observable({
-	firstName: 'Eric',
-	lastName: 'Bailey',
-	meta: {
-		birthday: '1992-02-14'
-	}
+var test = form({
+  outer: '.js-form',
+  inputs: '.js-input'
 });
 
-// Add callbacks for a few properties
-user.watch('firstName', function(val){
-	console.log(val)
-});
-user.watch('firstName', function(val){
-	console.log(val+' '+user.lastName)
-});
-user.watch('meta', function(val){
-	console.log(val)
-});
-
-// Set new property values
-setTimeout(function(){
-  user.firstName = 'Ryan';
-  user.meta = {
-    birthday: '1990-05-08'
-  };
-}, 1000);
-
-// Result
-console.dir(user);
+console.dir(test);
