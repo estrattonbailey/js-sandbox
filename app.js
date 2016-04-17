9dd1beb5f14a1b978db4159e608740d86880f4b3
Eric Bailey <ericstrattonbailey@gmail.com>
2016-04-16 20:43:20 -0400

    Events and bucket working

--- app.js
+++ app.js
@@ -42,103 +42,261 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	var del = document.documentElement || {};
-	var _match = (del.matches || del.webkitMatchesSelector || del.mozMatchesSelector || del.oMatchesSelector || del.msMatchesSelector || function(){return false;});
+	var event = __webpack_require__(4);
 
-	var buckets = {};
-
-	var methods = {
-	  on: function(event, target, callback){
-	    this.bind.call(this, event, target, callback)
-	  },
-	  bind: function(event, target, callback){
-	    if (!this.node){
-	      return;
-	    }
-
-	    if (!callback && typeof(target) == 'function'){
-	      callback = target;
-	      target = 'body';
-	    }
-
-	    this.event = event;
-	    this.target = target;
-	    this.callback = callback;
-
-	    this.node.addEventListener(event, this.handleEvent.bind(this), false);
-	  },
-	  send: function(){
-	    if (buckets[this.path]){
-	      buckets[this.path].push(this)
-	    } else {
-	      buckets[this.path] = [this]
-	    }
-
-	    this.callback(event);
-	  },
-	  off: function(){
-	    console.log('off')
-	    this.node.removeEventListener(this.event, this.handleEvent, false)
-	  },
-	  handleEvent: function(){
-	    event.currentTarget = this.node;
-
-	    if (this.findTarget(event)){
-	      this.send()
-	    } else {
-	      console.log('Nope')
-	    }
-	  },
-		findTarget: function(e){
-			var search = e.target;
-
-	    if (_match.call(search, this.target)){
-	      return true
-	    } else {
-	      for (; search;) {
-	        search = search.parentNode;
-
-	        // If it finds a match
-	        if (_match.call(search, this.target)){
-	          return true;
-	        }
-
-	        // If it doesn't, it will search up to the parent context
-	        if (search === e.currentTarget){
-	          return false;
-	        }
-	      }
-	    }
-		}
-	}
-	function listen(node){
-	  var _listen = Object.create(methods, {
-	    node: {
-	      value: node || null
-	    },
-	    path: {
-	      value: window.location.href 
-	    }
-	  });
-
-	  return _listen;
-	}
-
-	var outer = document.querySelector('.js-outer');
-
-	var test = listen(outer);
-
-	test.on('click', '.js-button', function(e){
-	  console.log(e)
-	  console.log(buckets)
+	event.on(document, '.js-button', 'click', function(e){
+	  console.log(this, e)
+	});
+	event.on('.js-button-2', 'click', function(e){
+	  console.log(this, e)
 	});
 
+
 	setTimeout(function(){
-	  test.off();
+	  event.off();
 	}, 5000);
 
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var closest = __webpack_require__(2);
+
+	/**
+	 * Delegates event to a selector.
+	 *
+	 * @param {Element} element
+	 * @param {String} selector
+	 * @param {String} type
+	 * @param {Function} callback
+	 * @param {Boolean} useCapture
+	 * @return {Object}
+	 */
+	function delegate(element, selector, type, callback, useCapture) {
+	    var listenerFn = listener.apply(this, arguments);
+
+	    element.addEventListener(type, listenerFn, useCapture);
+
+	    return {
+	        destroy: function() {
+	            element.removeEventListener(type, listenerFn, useCapture);
+	        }
+	    }
+	}
+
+	/**
+	 * Finds closest match and invokes callback.
+	 *
+	 * @param {Element} element
+	 * @param {String} selector
+	 * @param {String} type
+	 * @param {Function} callback
+	 * @return {Function}
+	 */
+	function listener(element, selector, type, callback) {
+	    return function(e) {
+	        e.delegateTarget = closest(e.target, selector, true);
+
+	        if (e.delegateTarget) {
+	            callback.call(element, e);
+	        }
+	    }
+	}
+
+	module.exports = delegate;
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var matches = __webpack_require__(3)
+
+	module.exports = function (element, selector, checkYoSelf) {
+	  var parent = checkYoSelf ? element : element.parentNode
+
+	  while (parent && parent !== document) {
+	    if (matches(parent, selector)) return parent;
+	    parent = parent.parentNode
+	  }
+	}
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	
+	/**
+	 * Element prototype.
+	 */
+
+	var proto = Element.prototype;
+
+	/**
+	 * Vendor function.
+	 */
+
+	var vendor = proto.matchesSelector
+	  || proto.webkitMatchesSelector
+	  || proto.mozMatchesSelector
+	  || proto.msMatchesSelector
+	  || proto.oMatchesSelector;
+
+	/**
+	 * Expose `match()`.
+	 */
+
+	module.exports = match;
+
+	/**
+	 * Match `el` to `selector`.
+	 *
+	 * @param {Element} el
+	 * @param {String} selector
+	 * @return {Boolean}
+	 * @api public
+	 */
+
+	function match(el, selector) {
+	  if (vendor) return vendor.call(el, selector);
+	  var nodes = el.parentNode.querySelectorAll(selector);
+	  for (var i = 0; i < nodes.length; ++i) {
+	    if (nodes[i] == el) return true;
+	  }
+	  return false;
+	}
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var delegate = __webpack_require__(1),
+	    sektor = __webpack_require__(5);
+
+	var events = [];
+
+	function addListener(target, event, callback){
+	  target = sektor(target) 
+
+	  for (var i = 0; i < target.length; i++){
+	    target[i].addEventListener(event, callback);
+	  }
+
+	  return {
+	    destroy: function(){
+	      for (var i = 0; i < target.length; i++){
+	        target[i].removeEventListener(event, callback)
+	      }
+	    }
+	  }
+	}
+
+	var Event = {
+	  on: function(root, target, event, callback){
+	    if (typeof event === 'function'){
+	      callback = event;
+	      event = target;
+	      target = root; 
+	      events.push(addListener(target, event, callback))
+	    } else {
+	      root = sektor(root) 
+	      for (var i = 0; i < root.length; i++){
+	        events.push(delegate(root[i], target, event, callback))
+	      }
+	    }
+	    console.log(events)
+	  },
+	  off: function(){
+	    for (var i = 0; i < events.length; i++){
+	      events[i].destroy();
+	    }
+	  }
+	}
+
+	module.exports = Event;
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	/* WEBPACK VAR INJECTION */(function(global) {'use strict';
+
+	var expando = 'sektor-' + Date.now();
+	var rsiblings = /[+~]/;
+	var document = global.document;
+	var del = document.documentElement || {};
+	var match = (
+	  del.matches ||
+	  del.webkitMatchesSelector ||
+	  del.mozMatchesSelector ||
+	  del.oMatchesSelector ||
+	  del.msMatchesSelector ||
+	  never
+	);
+
+	module.exports = sektor;
+
+	sektor.matches = matches;
+	sektor.matchesSelector = matchesSelector;
+
+	function qsa (selector, context) {
+	  var existed, id, prefix, prefixed, adapter, hack = context !== document;
+	  if (hack) { // id hack for context-rooted queries
+	    existed = context.getAttribute('id');
+	    id = existed || expando;
+	    prefix = '#' + id + ' ';
+	    prefixed = prefix + selector.replace(/,/g, ',' + prefix);
+	    adapter = rsiblings.test(selector) && context.parentNode;
+	    if (!existed) { context.setAttribute('id', id); }
+	  }
+	  try {
+	    return (adapter || context).querySelectorAll(prefixed || selector);
+	  } catch (e) {
+	    return [];
+	  } finally {
+	    if (existed === null) { context.removeAttribute('id'); }
+	  }
+	}
+
+	function sektor (selector, ctx, collection, seed) {
+	  var element;
+	  var context = ctx || document;
+	  var results = collection || [];
+	  var i = 0;
+	  if (typeof selector !== 'string') {
+	    return results;
+	  }
+	  if (context.nodeType !== 1 && context.nodeType !== 9) {
+	    return []; // bail if context is not an element or document
+	  }
+	  if (seed) {
+	    while ((element = seed[i++])) {
+	      if (matchesSelector(element, selector)) {
+	        results.push(element);
+	      }
+	    }
+	  } else {
+	    results.push.apply(results, qsa(selector, context));
+	  }
+	  return results;
+	}
+
+	function matches (selector, elements) {
+	  return sektor(selector, null, null, elements);
+	}
+
+	function matchesSelector (element, selector) {
+	  return match.call(element, selector);
+	}
+
+	function never () { return false; }
+
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ }
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,94 +1,13 @@
-var del = document.documentElement || {};
-var _match = (del.matches || del.webkitMatchesSelector || del.mozMatchesSelector || del.oMatchesSelector || del.msMatchesSelector || function(){return false;});
+var event = require('./event.js');
 
-var buckets = {};
-
-var methods = {
-  on: function(event, target, callback){
-    this.bind.call(this, event, target, callback)
-  },
-  bind: function(event, target, callback){
-    if (!this.node){
-      return;
-    }
-
-    if (!callback && typeof(target) == 'function'){
-      callback = target;
-      target = 'body';
-    }
-
-    this.event = event;
-    this.target = target;
-    this.callback = callback;
-
-    this.node.addEventListener(event, this.handleEvent.bind(this), false);
-  },
-  send: function(){
-    if (buckets[this.path]){
-      buckets[this.path].push(this)
-    } else {
-      buckets[this.path] = [this]
-    }
-
-    this.callback(event);
-  },
-  off: function(){
-    console.log('off')
-    this.node.removeEventListener(this.event, this.handleEvent, false)
-  },
-  handleEvent: function(){
-    event.currentTarget = this.node;
-
-    if (this.findTarget(event)){
-      this.send()
-    } else {
-      console.log('Nope')
-    }
-  },
-	findTarget: function(e){
-		var search = e.target;
-
-    if (_match.call(search, this.target)){
-      return true
-    } else {
-      for (; search;) {
-        search = search.parentNode;
-
-        // If it finds a match
-        if (_match.call(search, this.target)){
-          return true;
-        }
-
-        // If it doesn't, it will search up to the parent context
-        if (search === e.currentTarget){
-          return false;
-        }
-      }
-    }
-	}
-}
-function listen(node){
-  var _listen = Object.create(methods, {
-    node: {
-      value: node || null
-    },
-    path: {
-      value: window.location.href 
-    }
-  });
-
-  return _listen;
-}
-
-var outer = document.querySelector('.js-outer');
-
-var test = listen(outer);
-
-test.on('click', '.js-button', function(e){
-  console.log(e)
-  console.log(buckets)
+event.on(document, '.js-button', 'click', function(e){
+  console.log(this, e)
+});
+event.on('.js-button-2', 'click', function(e){
+  console.log(this, e)
 });
 
+
 setTimeout(function(){
-  test.off();
+  event.off();
 }, 5000);
